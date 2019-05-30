33ba7df8e6a95b1573f46e7c53dabdddac8b1563
guodd <guodd369@163.com>
2019-05-30 10:35:03 +0800

    Boot

---
+++ boot-03/boot-03-03/src/main/java/cn/hacz/edu/Boot0303Application.java
@@ -0,0 +1,20 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * project -
+ *
+ * @author guodd
+ * @version 1.0
+ * @date 日期:2019/5/30 时间:10:17
+ * @JDK 1.8
+ * @Description 功能模块：
+ */
+@SpringBootApplication
+public class Boot0303Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot0303Application.class, args);
+    }
+}
